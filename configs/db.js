fc8360fe6aa329325ee9793c150321dd2042514f
unknown <one.odin@yahoo.com>
2024-05-29 18:51:02 +0330

    initial commit

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,7 @@ const connectToDB = async () => {
     if (mongoose.connections[0].readyState) {
       return true;
     } else {
-      await mongoose.connect(process.env.MONGO_URL);
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log("Connect To DB Successfully ;)");
     }
   } catch (err) {
